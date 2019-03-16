08f2b7f19384420f8a0f0ce29caa0605d2a84971
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2019-03-17 01:43:03 +0900

    use socket.io-redis

--- src/socket.ts
+++ src/socket.ts
@@ -1,16 +1,28 @@
-import socketio from "socket.io";
+import socketIo from "socket.io";
+import socketIoRedis from "socket.io-redis";
+import os from "os"
 
 export default (server: any) => {
-  let io = socketio(server, {path: "/ws"});
+  let io = socketIo(server, {path: "/ws"});
+  io.adapter(socketIoRedis({host: '127.0.0.1', port: 6379}));
 
   io.on("connection", function(socket: any) {
-    console.log("connected");
+    console.log(`connected: ${os.hostname()} connected, id: ${socket.id}`);
+
+    socket.on("ping", function(message: any) {
+      console.log("pong");
+
+      socket.broadcast.emit("message", "pong");
+    });
 
     socket.on("message", function(message: any) {
       console.log(message);
 
-      socket.emit("message", `pong ${message}`);
+      socket.broadcast.emit("message", `${message}`);
     });
 
+    socket.on('disconnect', () => {
+      console.log(`disconnected, id: ${socket.id}`);
+    });
   });
 }
