cc494186e160a56ba9910051d0245b99991f138f
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2019-03-17 20:39:32 +0900

    Change conding syntax

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import path from "path";
 import http from "http";
-import socket from "./socket";
+import wsserver from "./socket";
 import SubscribeCapture from "./subscribeCapture";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const httpServer = new http.Server(app);
-const io = socket(httpServer);
+const ws = new wsserver(httpServer);
 const capture = new SubscribeCapture();
 capture.start();
 
--- src/socket.ts
+++ src/socket.ts
@@ -2,36 +2,41 @@ import socketIo from "socket.io";
 import redisAdapter from "socket.io-redis";
 import os from "os";
 
-export default (server: any) => {
-  const REDIS_HOST: string = process.env["REDIS_HOST"] || "127.0.0.1";
-  const REDIS_PORT = 6379;
+export default class WSServer {
+  private io: socketIo.Server;
 
-  const io = socketIo(server, { path: "/ws" });
-  const adapter = redisAdapter({ host: REDIS_HOST, port: REDIS_PORT });
-  adapter.pubClient.on("error", () => {
-    console.log("Redis connection error");
-  });
-  io.adapter(adapter);
+  public constructor(server: any) {
+    const REDIS_HOST: string = process.env["REDIS_HOST"] || "127.0.0.1";
+    const REDIS_PORT = 6379;
 
-  io.on("connection", (socket: any) => {
-    console.log(`connected: ${os.hostname()} connected, id: ${socket.id}`);
-
-    socket.on("ping", (message: any) => {
-      console.log("pong");
-
-      socket.broadcast.emit("message", "pong");
+    this.io = socketIo(server, { path: "/ws" });
+    const adapter = redisAdapter({ host: REDIS_HOST, port: REDIS_PORT });
+    adapter.pubClient.on("error", () => {
+      console.log("Redis connection error");
     });
+    this.io.adapter(adapter);
+    this.initSocket();
+  }
 
-    socket.on("message", (message: any) => {
-      console.log(message);
+  private initSocket() {
+    this.io.on("connection", (socket: any) => {
+      console.log(`connected: ${os.hostname()} connected, id: ${socket.id}`);
 
-      socket.broadcast.emit("message", `${message}`);
+      socket.on("ping", (message: any) => {
+        console.log("pong");
+
+        socket.broadcast.emit("message", "pong");
+      });
+
+      socket.on("message", (message: any) => {
+        console.log(message);
+
+        socket.broadcast.emit("message", `${message}`);
+      });
+
+      socket.on("disconnect", () => {
+        console.log(`disconnected, id: ${socket.id}`);
+      });
     });
-
-    socket.on("disconnect", () => {
-      console.log(`disconnected, id: ${socket.id}`);
-    });
-  });
-
-  return io;
-};
+  }
+}
